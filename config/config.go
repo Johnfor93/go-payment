d987625698bb149558a91052383bf940090009c2
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-07-12 09:29:27 +0700

    env reader

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+
+	"log"
+)
+
+func envVariable(key string) string {
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+    log.Fatalf("Error while reading config file %s", err)
+  }
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+  return value
+}
--- controllers/payment.go
+++ controllers/payment.go
@@ -2,6 +2,7 @@ package paymentcontroller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/Johnfor93/go-payment/config"
 
 	"crypto/sha256"
 	"encoding/base64"
@@ -12,14 +13,17 @@ import (
 
 func Create(ctx *gin.Context){
 	timestamp := time.Now()
-
+	merchantid := config.envVariable("MERCHANT_ID")
+	merchantkey := config.envVariable("MERCHANT_KEY")
+	
 	convertTime := timestamp.Format("20060102150405")
 
 	h := sha256.New()
-	h.Write([]byte(convertTime))
+	h.Write([]byte(convertTime+merchantid+merchantkey))
 	fmt.Printf("Time now is %v", convertTime)
-
+	
 	sha := base64.URLEncoding.EncodeToString(h.Sum(nil))
+	fmt.Printf("The SHA Encoding is %v", sha)
 
 	ctx.JSON(http.StatusOK, gin.H{"hash": sha})
 }
